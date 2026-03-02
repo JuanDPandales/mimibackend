9c6d2a785fe024ef4ba36e92566536770059ad07
Juan Pandales <89496222+JuanDPandales@users.noreply.github.com>
2026-03-02 04:34:31 -0500

    No estaba usando esto.

--- src/shared/audit.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentGatewayService } from './payment-gateway/payment-gateway.service';
-
-@Module({
-  providers: [PaymentGatewayService],
-})
-export class AuditModule {}
