559b2ad3737d24ad4c0172f2a17a42af66be16fd
Juan Pandales <89496222+JuanDPandales@users.noreply.github.com>
2026-03-02 07:35:13 -0500

    SSL to false

--- src/shared/database/data-source.ts
+++ src/shared/database/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production'
-        ? { rejectUnauthorized: true }
+        ? { rejectUnauthorized: false }
         : {
             rejectUnauthorized: false,
         },
